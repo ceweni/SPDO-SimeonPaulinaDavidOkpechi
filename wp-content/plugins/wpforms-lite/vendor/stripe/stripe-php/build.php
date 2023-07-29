1cdae90744b6cc8581795d83ee4ef3daf305ed9b
Chukwunonso Eweni <ewenai6@gmail.com>
2023-07-29 03:01:05 -0500

    updated contact page

--- wp-content/plugins/wpforms-lite/vendor/stripe/stripe-php/build.php
+++ wp-content/plugins/wpforms-lite/vendor/stripe/stripe-php/build.php
@@ -3,5 +3,3 @@
 <b>Warning</b>:  Undefined variable $argv in <b>/Users/chukwunonsoeweni/Local Sites/spdo/app/public/wp-content/plugins/wpforms-lite/vendor/stripe/stripe-php/build.php</b> on line <b>5</b><br>
 <br>
 <b>Warning</b>:  Trying to access array offset on value of type null in <b>/Users/chukwunonsoeweni/Local Sites/spdo/app/public/wp-content/plugins/wpforms-lite/vendor/stripe/stripe-php/build.php</b> on line <b>5</b><br>
-<br>
-<b>Warning</b>:  file_get_contents(composer.json): Failed to open stream: No such file or directory in <b>/Users/chukwunonsoeweni/Local Sites/spdo/app/public/wp-content/plugins/wpforms-lite/vendor/stripe/stripe-php/build.php</b> on line <b>10</b><br>
